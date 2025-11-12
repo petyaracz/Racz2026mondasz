8f849a1eea2d8747a06309d80e6f267cc424b0fd
Péter Rácz <petermartonracz@gmail.com>
2025-11-12 09:06:39 +0100

    abrak

--- script/doodle.R
+++ script/doodle.R
@@ -3,12 +3,30 @@
 setwd('~/Github/Racz2026mondasz/')
 
 library(tidyverse)
+library(patchwork)
 
 # -- read -- #
 
-d = read_tsv('dat/exp_data_tidy.tsv')
+c = read_tsv('dat/mondasz_training.tsv')
+d = read_tsv('dat/exp_data_tidy.tsv.gz')
 
 # -- doodle -- #
+
+## c
+
+p1 = c |> 
+  # filter(coda %in% c('jt','ng','nt','st')) |> 
+  mutate(p_v = freq_v/(freq_v+freq_nv)) |> 
+  ggplot(aes(p_v,tag,colour = coda)) +
+  geom_boxplot() +
+  xlab('p(mondas)') +
+  ylab('') +
+  theme_bw() +
+  scale_colour_viridis_d(option = 'F') +
+  ggtitle('corpus') +
+  theme(legend.position = 'bottom')
+
+## d
 
 my_counts = d |> 
   count(resp_v,lemma_orth,tag,coda) |> 
@@ -22,3 +40,17 @@ my_counts |>
 my_counts |> 
   ggplot(aes(p_v,coda,colour = tag)) +
   geom_boxplot()
+
+p2 = my_counts |> 
+  ggplot(aes(p_v,tag,colour = coda)) +
+  geom_boxplot() +
+  xlab('p(mondas)') +
+  ylab('') +
+  theme_bw() +
+  scale_colour_viridis_d(option = 'H') +
+  ggtitle('experiment') +
+  theme(axis.text.y = element_blank()) +
+  theme(legend.position = 'bottom')
+
+p1 + p2
+ggsave('viz/res.png', dpi = 'print', width = 10, height = 6)
